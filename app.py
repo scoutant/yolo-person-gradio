ed4ad053c3c2fa89854119d4ceacff4451464509
Coutant Stéphane <stephane@coutant.xyz>
2022-09-20 15:26:32 +0200

    removed warnings.

--- app.py
+++ app.py
@@ -17,18 +17,21 @@ def inference(img:PIL.Image.Image, threshold:float=0.6):
     model.conf = threshold
     # detections:torchvision.Detections = model(images, size=640)
     detections = model(images, size=640)
+    print( "detections type:" , type(detections))
+    print( "attributes:" , dir(detections))
     predictions:torch.Tensor = detections.pred[0] # the predictions for our single image
+    result_image=detections.imgs[0]
     detections.render() # bounding boxes and labels added into image
-    return detections.imgs[0], predictions.size(dim=0) # image and number of detections
+    # return detections.imgs[0], predictions.size(dim=0) # image and number of detections
+    return result_image, predictions.size(dim=0) # image and number of detections
 
 gr.Interface(
     fn = inference,
-    inputs = [ gr.inputs.Image(type="pil", label="Input"), gr.Slider(minimum=0.5, maximum=0.9, step=0.05, value=0.7, label="Confidence threshold") ],
-    outputs = [ gr.components.Image(type="pil", label="Output"), gr.components.Label(label="nb of persons detected for given confidence threshold") ],
+    inputs = [ gr.Image(type="pil", label="Input"), gr.Slider(minimum=0.5, maximum=0.9, step=0.05, value=0.7, label="Confidence threshold") ],
+    outputs = [ gr.Image(type="pil", label="Output"), gr.Label(label="nb of persons detected for given confidence threshold") ],
     title="Person detection with YOLO v5",
     description="Person detection, you can twik the corresponding confidence threshold. Good results even when face not visible.",
     article=article,
     examples=[['data/businessmen-612.jpg'], ['data/businessmen-back.jpg']],
-    enable_queue=True,
     allow_flagging="never"
-).launch(debug=True)
+).launch(debug=True, enable_queue=True, share=True)
